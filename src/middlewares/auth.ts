009d05809eb7a32fd5e7f940f8d90d0aada2cad4
unknown <ksh03196@stu.woosuk.ac.kr>
2025-07-31 14:43:49 +0900

    fix(auth):expressjwt함수의 secret에 null&undefined값이 들어갈수 없도록 수정

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,7 +1,16 @@
 import { expressjwt } from 'express-jwt';
+import { Request } from 'express';
 
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET
+
+if (!ACCESS_TOKEN_SECRET) {
+  throw new Error('환경 변수 ACCESS_TOKEN_SECRET이 설정되지 않았습니다.');
+}
+if (!REFRESH_TOKEN_SECRET) {
+  throw new Error('환경 변수 REFRESH_TOKEN_SECRET이 설정되지 않았습니다.');
+}
+
 // Access Token 검증 미들웨어
 export const verifyAccessToken = expressjwt({
   secret: ACCESS_TOKEN_SECRET,
@@ -13,7 +22,7 @@ export const verifyAccessToken = expressjwt({
 export const verifyRefreshToken = expressjwt({
   secret: REFRESH_TOKEN_SECRET,
   algorithms: ['HS256'],
-  getToken: (req) => {
+  getToken: (req: Request) => {
     if (req.cookies && req.cookies.refreshToken) {
       return req.cookies.refreshToken;
     }
