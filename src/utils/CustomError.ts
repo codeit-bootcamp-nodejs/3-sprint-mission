d6bf818f666fc6aeb862e5f87e74a34711ea6b0a
selentia <admin@duna.me>
2025-08-05 18:54:11 +0900

    final: prepare sprint6 for submission

--- src/utils/CustomError.ts
+++
@@ -1,9 +0,0 @@
-export class CustomError extends Error {
-  status: number;
-
-  constructor(message: string, status = 500) {
-    super(message);
-    this.status = status;
-    this.name = 'CustomError';
-  }
-}
