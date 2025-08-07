f1f1663ad1070df97f3141fb3421f1ac0cf45b71
unknown <ksh03196@stu.woosuk.ac.kr>
2025-08-07 16:52:27 +0900

    fix(comment):try catch 중복 피드백 반영

--- src/controllers/productCommentController.ts
+++ src/controllers/productCommentController.ts
@@ -3,61 +3,40 @@ import * as productCommentService from '../services/productCommentService';
 
 // 댓글 생성 컨트롤러
 export const createCommentController = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const { productId } = req.params;
-    const { content } = req.body;
-    const userId = req.user!.userId;
-
-    const newComment = await productCommentService.createProductComment({
-      productId,
-      content,
-      userId,
-    });
-
-    res.status(201).json(newComment);
-  } catch (error) {
-    next(error);
-  }
+  const { productId } = req.params;
+  const { content } = req.body;
+  const userId = req.user!.userId;
+  const newComment = await productCommentService.createProductComment({
+    productId,
+    content,
+    userId,
+  });
+  res.status(201).json(newComment);
 };
 
 
 // 댓글 목록 조회 컨트롤러
 export const getCommentsController = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const productId = req.params.productId;
-    const cursor = req.query.cursor as string | undefined;
-    const limit = req.query.limit as string | undefined;
-
-    const comments = await productCommentService.findAllProductComments({ productId, cursor, limit });
-    res.status(200).json(comments);
-  } catch (error) {
-    next(error);
-  }
+  const productId = req.params.productId;
+  const cursor = req.query.cursor as string | undefined;
+  const limit = req.query.limit as string | undefined;
+  const comments = await productCommentService.findAllProductComments({ productId, cursor, limit });
+  res.status(200).json(comments);
 };
 
 // 댓글 수정 컨트롤러
 export const updateCommentController = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const commentId = req.params.commentId;
-    const { content } = req.body;
-    const userId = req.user!.userId;
-
-    const updatedComment = await productCommentService.updateProductComment(commentId, { content, userId });
-    res.status(200).json(updatedComment);
-  } catch (error) {
-    next(error);
-  }
+  const commentId = req.params.commentId;
+  const { content } = req.body;
+  const userId = req.user!.userId;
+  const updatedComment = await productCommentService.updateProductComment(commentId, { content, userId });
+  res.status(200).json(updatedComment);
 };
 
 // 댓글 삭제 컨트롤러
 export const deleteCommentController = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const commentId = req.params.commentId;
-    const userId = req.user!.userId;
-
-    await productCommentService.deleteProductComment(commentId, userId);
-    res.status(204).send();
-  } catch (error) {
-    next(error);
-  }
+  const commentId = req.params.commentId;
+  const userId = req.user!.userId;
+  await productCommentService.deleteProductComment(commentId, userId);
+  res.status(204).send();
 };
