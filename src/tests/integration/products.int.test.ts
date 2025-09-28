b18930b0eced59709e6c8078ac9450cc01d5dd7e
luciakim22 <luciakim22@naver.com>
2025-09-28 21:30:24 +0900

    init commit

---
+++ src/tests/integration/products.int.test.ts
@@ -0,0 +1,89 @@
+import request from 'supertest';
+import app from '../../main';
+import { prismaClient } from '../../lib/prismaClient';
+
+afterAll(async () => {
+  await prismaClient.$disconnect()
+})
+
+describe('Products API (Integration)', () => {
+  // 인증 필요 없는 API
+  describe('GET /products', () => {
+    it('상품 목록을 반환한다.', async () => {
+      const res = await request(app).get('/products');
+      expect(res.status).toBe(200);
+      expect(Array.isArray(res.body.list)).toBe(true);
+    });
+  });
+
+  // 인증 필요한 API
+  describe('POST /products', () => {
+    test('인증 없이는 상품 등록이 실패한다', async () => {
+      const res = await request(app)
+        .post('/products')
+        .send({ name: '노트북', description: '삼성 갤럭시', price: 500 });
+      expect(res.status).toBe(401);
+    });
+
+    test('인증 후 상품을 등록할 수 있다.', async () => {
+      //로그인해서 토큰 등록받기
+      const loginRes = await request(app)
+        .post('/auth/login')
+        .send({ email: 'test@example.com', password: 'password' });
+
+      expect(loginRes.status).toBe(200);
+
+      const token = loginRes.body.token;
+
+      const res = await request(app)
+        .post('/products')
+        .set('Authorization', `Bearer ${token}`)
+        .send({ name: '노트북', description: '삼성 갤럭시', price: 500 });
+
+      expect(res.status).toBe(201)
+      expect(res.body).toHaveProperty('id')
+      expect(res.body.name).toBe('노트북')
+    });
+  });
+});
+
+describe('Articles API (Integration)', () => {
+  // 인증 필요 없는 API
+  describe('GET /articles', () => {
+    it('상품 목록을 반환한다.', async () => {
+      const res = await request(app).get('/articles');
+      expect(res.status).toBe(200);
+      expect(Array.isArray(res.body.list)).toBe(true);
+    });
+  });
+
+  // 인증 필요한 API
+  describe('POST /articles', () => {
+    test('인증 없이는 게시글 등록이 실패한다', async () => {
+      const res = await request(app)
+        .post('/articles')
+        .send({ name: '노트북', description: '삼성 갤럭시', price: 500 });
+      expect(res.status).toBe(401);
+    });
+
+//     test('인증 후 게시글을 등록할 수 있다.', async () => {
+//       //로그인해서 토큰 등록받기
+//       const loginRes = await request(app)
+//         .post('/login')
+//         .send({ email: 'test@example.com', password: 'password' });
+
+//       expect(loginRes.status).toBe(200);
+
+//       const token = loginRes.body.token;
+
+//       const res = await request(app)
+//         .post('/products')
+//         .set('Authorization', `Bearer ${token}`)
+//         .send({ name: '노트북', description: '삼성 갤럭시', price: 500 });
+
+//       expect(res.status).toBe(201)
+//       expect(res.body).toHaveProperty('id')
+//       expect(res.body.name).toBe('노트북')
+//     });
+  });
+});
